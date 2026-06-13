b998cef70477035930bbf399f3e8a9cc889c9d01
Patrick Serrano <patrickcmserrano@gmail.com>
2026-06-13 12:23:32 -0300

    feat: rebuild dev diaries conforming to generate_weekly_report_skill.md and include remote repos

--- build/_app/immutable/entry/start.Bs338T4Y.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/OE9ij3Er.js";export{o as load_css,r as start};
---
+++ build/_app/immutable/entry/start.DcjzzYnX.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/COKC0Ncw.js";export{o as load_css,r as start};
